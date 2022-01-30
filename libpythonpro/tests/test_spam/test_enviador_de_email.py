cf8801b040b8df1dd283e75c27560f476280cdbf
Flavio <flavioantonio5@hotmail.com>
2022-01-30 16:55:21 -0300

    testeexcesaoo

--- libpythonpro/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -1,11 +1,12 @@
 import pytest
 
-from libpythonpro.spam.enviador_de_email import Enviador
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
 
 
 def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
+
 
 @pytest.mark.parametrize(
     'destinatario',
@@ -19,5 +20,20 @@ def test_remetente(destinatario):
         'luciano@python.pro.br',
         'Cursos Python Pro',
         'Primeira turma Guido Von osumm aberta.'
-        )
+    )
     assert destinatario in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'renzo']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'luciano@python.pro.br',
+            'Cursos Python Pro',
+            'Primeira turma Guido Von osumm aberta.'
+        )
